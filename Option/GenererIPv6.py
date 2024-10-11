e81b0287b65c56d3546d28eb52fb46959613c8f7
juto222 <thomasjulien3101@gmail.com>
2024-10-11 10:13:03 +0200

    GenererIPv6.py

--- Option/GenererIPv6.py
+++ Option/GenererIPv6.py
@@ -1,9 +1,13 @@
 import random
-
+import ipaddress
 
 def generate_ipv6():
-    ipv6 = ":".join(f"{random.randint(0, 65535):x}" for _ in range(8))
-    print("Votre nouvelle IP générée est : ", ipv6)
+    ipv6_1 = random.randint(0,255)
+    ipv6_2 = random.randint(0,255)
+    ipv6_3 = random.randint(0,255)
+    ipv6_4 = random.randint(0,255)
+
+    print("Votre IP généré est ",ipv6_1,".",ipv6_2,".",ipv6_3,".",ipv6_4)
 
 
 
