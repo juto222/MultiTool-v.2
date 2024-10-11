60ea2c6c7963f250bb7199641e61cde801cafdff
juto222 <thomasjulien3101@gmail.com>
2024-10-11 15:35:53 +0200

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -11,18 +11,3 @@ elif os.name == 'posix':
     os.system(f'open {main.py}')  
 
 
-
-if sys.platform.startswith("win"):
-    os.system("python -m pip install -r requirements.txt")
-    os.system("python main.py")
-
-elif sys.platform.startswith("linux"):
-    os.system("python3 -m pip3 install -r requirements.txt")
-    os.system("python3 main.py")
-
-
-if os.name == 'nt':  
-    os.startfile(main.py)
-elif os.name == 'posix':  
-    os.system(f'open {main.py}') 
-
