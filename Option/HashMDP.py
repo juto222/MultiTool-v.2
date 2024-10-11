7336b69fec50184baaa51fdc5d994ef1cee7ef0f
juto222 <thomasjulien3101@gmail.com>
2024-10-11 15:37:26 +0200

    Update HashMDP.py

--- Option/HashMDP.py
+++ Option/HashMDP.py
@@ -1,6 +1,18 @@
 import hashlib
 
 def hasher_mot_de_passe(mot_de_passe):
+    print(r"""
+             ______   _____    ______                              _____       _  _      _   __   ___ 
+            |  ___ \ (____ \  (_____ \                   _  _     / ___ \     | || |    | | /  | / __)
+            | | _ | | _   \ \  _____) )   ___  ___  ___ ( \( \   ( (   ) )  _ | || |  _ | |/_/ || |__ 
+            | || || || |   | ||  ____/   (___)(___)(___) ) )) )   > > < <  / || || | / )| |  | ||  __)
+            | || || || |__/ / | |                       (_/(_/   ( (___) )( (_| || |< ( | |  | || |   
+            |_||_||_||_____/  |_|                                 \_____/  \____||_| \_)|_|  |_||_|   
+          
+
+
+                                                                                          
+    """)
     mot_de_passe = input("Entrez le mot de passe à hasher : ")
     mot_de_passe_hash = hasher_mot_de_passe(mot_de_passe)
     mot_de_passe_bytes = mot_de_passe.encode()
