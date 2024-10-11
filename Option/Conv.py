4979d71f5c73a442b26f39b9d67ad50f9729f078
juto222 <thomasjulien3101@gmail.com>
2024-10-11 10:04:02 +0200

    Conv.py

--- Option/Conv.py
+++ Option/Conv.py
@@ -1,11 +1,10 @@
 import ipaddress
 
 def convertir():
-    # Demande à l'utilisateur d'entrer une adresse IPv6 en hexadécimal
-    ipv6_hex = input("Entrez une adresse IPv6 en hexadécimal (ex: 20010db8000000000000000000000001) : ")
+    
+    ipv6_hex = input("Entrez une adresse IPv6 en hexadécimal (ex: 20010dbc29999999999999999999991) : ")
 
     try:
-        # Convertit la chaîne hexadécimale en adresse IPv6
         ipv6_normal = ipaddress.IPv6Address(ipv6_hex)
         print("Adresse IPv6 au format normal :", ipv6_normal)
     except ValueError:
