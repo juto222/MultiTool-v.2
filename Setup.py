c6deff4d5c0291cd5ce9960145e5c27b55e1c41b
juto222 <thomasjulien3101@gmail.com>
2024-10-23 00:04:58 +0200

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -9,4 +9,4 @@ os.system("pip install dnspython")
 os.system("pip install python-nmap")
 os.system("pip install requests")
 os.system("pip install pywin32")
-
+os.system("python -m pip install -r requirements.txt")
