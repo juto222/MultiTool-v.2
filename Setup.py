415c917041ba8153f9e711043a64e1404b56a955
juto222 <thomasjulien3101@gmail.com>
2024-10-22 23:39:51 +0200

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -8,5 +8,5 @@ print("Installation des modules pour mon tool:")
 os.system("pip install dnspython")
 os.system("pip install python-nmap")
 os.system("pip install requests")
-
+os.system("pip install pywin32")
 
